327f20131c82f971a928b900771e47288b2db951
YoEight <yo.eight@gmail.com>
2026-02-15 15:42:38 -0500

    compiles but tests are not passing

--- crates/vigil-core/src/queries/aggregates/mod.rs
+++ crates/vigil-core/src/queries/aggregates/mod.rs
@@ -7,83 +7,17 @@ use crate::{
     queries::Sources,
     values::QueryValue,
 };
-use case_insensitive_hashmap::CaseInsensitiveHashMap;
-use eventql_parser::prelude::Expr;
 use eventql_parser::{
-    App, ExprRef, Order, Query, Session, Value,
+    App, ExprRef, Query, Session, Value,
     prelude::{Type, Typed},
 };
 use std::collections::hash_map::Entry;
 use std::collections::{BTreeMap, HashMap};
-use std::vec;
+use std::{mem, vec};
 
 pub trait Aggregate {
     fn fold(&mut self, params: &[QueryValue]);
     fn complete(&self) -> QueryValue;
-}
-
-enum AggState {
-    Single(Agg),
-    Record(CaseInsensitiveHashMap<AggState>),
-}
-
-impl AggState {
-    fn from(session: &Session, query: &Query<Typed>) -> Self {
-        match session.arena().get_expr(query.projection).value {
-            eventql_parser::Value::Record(fields) => {
-                let mut aggs = CaseInsensitiveHashMap::new();
-
-                for field in session.arena().get_rec(fields) {
-                    let field_name = session.arena().get_str(field.name);
-                    if let Value::App(app) = session.arena().get_expr(field.expr).value {
-                        aggs.insert(
-                            field_name,
-                            Self::Single(instantiate_aggregate(session, &app)),
-                        );
-                        continue;
-                    }
-
-                    aggs.insert(field_name, Self::Single(Agg::unique()));
-                }
-
-                Self::Record(aggs)
-            }
-
-            eventql_parser::Value::App(app) => Self::Single(instantiate_aggregate(session, &app)),
-
-            _ => unreachable!("we expect an aggregate expression so this case should never happen"),
-        }
-    }
-
-    fn complete(&self) -> QueryValue {
-        match self {
-            AggState::Single(agg) => agg.complete(),
-            AggState::Record(aggs) => {
-                let mut props = BTreeMap::new();
-
-                for (key, agg) in aggs.iter() {
-                    props.insert(key.as_ref().to_owned(), agg.complete());
-                }
-
-                QueryValue::Record(props)
-            }
-        }
-    }
-}
-
-fn instantiate_ordered_aggregate<'a>(
-    session: &'a Session,
-    order_by: &'a eventql_parser::OrderBy,
-) -> Option<AggOrdered> {
-    let expr = session.arena().get_expr(order_by.expr);
-    if let eventql_parser::Value::App(app) = expr.value {
-        Some(AggOrdered {
-            expr,
-            agg: AggState::Single(instantiate_aggregate(session, &app)),
-        })
-    } else {
-        None
-    }
 }
 
 fn instantiate_aggregate(session: &Session, app: &App) -> Agg {
@@ -114,6 +48,7 @@ enum AggKind {
     Grouped {
         base: HashMap<App, Agg>,
         value: Value,
+        having: Option<ExprRef>,
         aggs: HashMap<QueryValue, HashMap<App, Agg>>,
     },
 }
@@ -138,6 +73,7 @@ impl AggKind {
             Self::Grouped {
                 base: aggs,
                 value: session.arena().get_expr(group_by.expr).value,
+                having: group_by.predicate,
                 aggs: Default::default(),
             }
         } else {
@@ -164,6 +100,7 @@ impl AggKind {
     }
 }
 
+#[derive(Default)]
 struct EvalAgg {
     buffer: Vec<QueryValue>,
 }
@@ -173,7 +110,9 @@ impl EvalAgg {
         match kind {
             AggKind::Regular(aggs) => self.fold_aggs(interpreter, aggs),
 
-            AggKind::Grouped { base, value, aggs } => {
+            AggKind::Grouped {
+                base, value, aggs, ..
+            } => {
                 let key = interpreter.eval(*value)?;
                 let aggs = aggs.entry(key).or_insert_with(|| base.clone());
 
@@ -212,11 +151,20 @@ impl EvalAgg {
                 self.buffer.push(value);
             }
 
-            AggKind::Grouped { aggs, .. } => {
+            AggKind::Grouped { aggs, having, .. } => {
+                let having = having.as_ref().copied();
+
                 if let Some(order) = query.order_by.map(|o| o.order) {
                     let mut orderer = QueryOrderer::new(order);
 
                     for (key, aggs) in aggs.iter() {
+                        if let Some(predicate) = having {
+                            let value = self.complete_aggs(interpreter, aggs, predicate)?;
+                            if !matches!(value, QueryValue::Bool(true)) {
+                                continue;
+                            }
+                        }
+
                         let value = self.complete_aggs(interpreter, aggs, query.projection)?;
                         orderer.insert(key.clone(), value);
                     }
@@ -273,6 +221,18 @@ impl EvalAgg {
                 Ok(QueryValue::Record(props))
             }
 
+            Value::Binary(binary) => {
+                let lhs = self.complete_aggs(interpreter, aggs, binary.lhs)?;
+                let rhs = self.complete_aggs(interpreter, aggs, binary.rhs)?;
+
+                interpreter.eval_binary(binary.operator, &lhs, &rhs)
+            }
+
+            Value::Unary(unary) => {
+                let value = self.complete_aggs(interpreter, aggs, unary.expr)?;
+                interpreter.eval_unary(unary.operator, &value)
+            }
+
             _ => Err(EvalError::Runtime(
                 "unreachable code path in aggregate computation".into(),
             )),
@@ -280,63 +240,28 @@ impl EvalAgg {
     }
 }
 
-enum Emit {
-    Single(AggState),
-    Grouped {
-        ordered: Option<Order>,
-        aggs: HashMap<String, AggGroup>,
-    },
-}
-
-struct AggGroup {
-    ordered: Option<AggOrdered>,
-    state: AggState,
-}
-
-impl AggGroup {
-    fn update_order_agg(&mut self, interpreter: &Interpreter) -> EvalResult<()> {
-        if let Some(agg) = &mut self.ordered {
-            eval_agg_value(interpreter, agg.expr.value, &mut agg.agg)?;
-        }
-
-        Ok(())
-    }
-}
-
-struct AggOrdered {
-    expr: Expr,
-    agg: AggState,
-}
-
 pub struct AggQuery<'a> {
     srcs: Sources<'a>,
     interpreter: Interpreter<'a>,
     query: Query<Typed>,
-    emit: Emit,
+    kind: AggKind,
+    agg_eval: EvalAgg,
     completed: bool,
-    session: &'a Session,
     results: vec::IntoIter<QueryValue>,
 }
 
 impl<'a> AggQuery<'a> {
     pub fn new(srcs: Sources<'a>, session: &'a Session, query: Query<Typed>) -> Self {
-        let emit = if query.group_by.is_some() {
-            Emit::Grouped {
-                ordered: query.order_by.as_ref().map(|o| o.order),
-                aggs: Default::default(),
-            }
-        } else {
-            Emit::Single(AggState::from(session, &query))
-        };
+        let kind = AggKind::load(session, &query);
 
         Self {
             srcs,
             query,
+            kind,
             interpreter: Interpreter::new(session),
-            emit,
-            session,
             completed: false,
             results: Default::default(),
+            agg_eval: Default::default(),
         }
     }
 }
@@ -358,36 +283,14 @@ impl<'a> Iterator for AggQuery<'a> {
                 outcome
             } else {
                 self.completed = true;
-
-                match &self.emit {
-                    Emit::Single(agg) => self.results = vec![agg.complete()].into_iter(),
-                    Emit::Grouped { ordered, aggs } => {
-                        let mut results = Vec::new();
-                        if let Some(order) = ordered {
-                            let mut orderer = QueryOrderer::new(*order);
-                            for group in aggs.values() {
-                                let key = if let Some(agg) = &group.ordered {
-                                    agg.agg.complete()
-                                } else {
-                                    QueryValue::Null
-                                };
-                                orderer.insert(key, group.state.complete())
-                            }
-
-                            orderer.prepare_for_streaming()?;
-                            while let Some(value) = orderer.next() {
-                                results.push(value);
-                            }
-                        } else {
-                            for group in aggs.values() {
-                                results.push(group.state.complete());
-                            }
-                        }
-
-                        self.results = results.into_iter();
-                    }
+                if let Err(e) =
+                    self.agg_eval
+                        .complete(&self.interpreter, &mut self.kind, &self.query)
+                {
+                    return Some(Err(e));
                 }
 
+                self.results = mem::take(&mut self.agg_eval.buffer).into_iter();
                 continue;
             };
 
@@ -401,53 +304,7 @@ impl<'a> Iterator for AggQuery<'a> {
                 Err(e) => return Some(Err(e)),
             }
 
-            let agg = if let Emit::Grouped { aggs, .. } = &mut self.emit
-                && let Some(group_by) = &self.query.group_by
-            {
-                let group_key = match self
-                    .interpreter
-                    .eval(self.session.arena().get_expr(group_by.expr).value)
-                {
-                    Err(e) => return Some(Err(e)),
-                    Ok(value) => match value {
-                        QueryValue::String(s) => s.clone(),
-                        QueryValue::Number(n) => n.to_string(),
-                        QueryValue::Bool(b) => b.to_string(),
-                        QueryValue::DateTime(date_time) => date_time.to_string(),
-                        QueryValue::Date(naive_date) => naive_date.to_string(),
-                        QueryValue::Time(naive_time) => naive_time.to_string(),
-                        _ => {
-                            return Some(Err(EvalError::Runtime(
-                                "unexpected group by value".into(),
-                            )));
-                        }
-                    },
-                };
-
-                let agg_group = aggs.entry(group_key).or_insert_with(|| AggGroup {
-                    ordered: self
-                        .query
-                        .order_by
-                        .as_ref()
-                        .and_then(|o| instantiate_ordered_aggregate(self.session, o)),
-                    state: AggState::from(self.session, &self.query),
-                });
-
-                if let Err(e) = agg_group.update_order_agg(&self.interpreter) {
-                    return Some(Err(e));
-                }
-
-                &mut agg_group.state
-            } else if let Emit::Single(agg) = &mut self.emit {
-                agg
-            } else {
-                return Some(Err(EvalError::Runtime(
-                    "wrong code path when running aggregate query".into(),
-                )));
-            };
-
-            let proj_expr = self.session.arena().get_expr(self.query.projection);
-            if let Err(e) = eval_agg_value(&self.interpreter, proj_expr.value, agg) {
+            if let Err(e) = self.agg_eval.fold(&self.interpreter, &mut self.kind) {
                 return Some(Err(e));
             }
         }
